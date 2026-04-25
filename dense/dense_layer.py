735b9cec27965c459bcd1630449854a821cdecac
Navnath Kadam <navnathkadam284@gmail.com>
2026-04-25 17:35:30 +0530

    feat: added feed forward neural network

---
+++ dense/dense_layer.py
@@ -0,0 +1,79 @@
+import numpy as np
+from activations.relu import Activation_ReLU
+from activations.sigmoid import Activation_Sigmoid
+from activations.leaky_relu import Activation_LeakyReLU
+from activations.tanh import Activation_Tanh
+from enum import Enum
+
+
+class Activations(Enum):
+    SIGMOID = "sigmoid"
+    RELU = "relu"
+    TANH = "tanh"
+    LEAKYRELU = "leaky_relu"
+
+
+class Dense_Layer:
+    """
+    NOTE:
+        -  My Idea is User will create Object of Dense_Layer (here one object means 1 layer of neurons)
+        - layer1 = Dense_Layer(4,5)
+        - NOTE: Here 4 is current layer neurons, 5 is next layer neurons this should be known to user
+
+    NOTE:
+        - Each Neurons has (len(n_inputs[0]) weights) and (1 bias)
+    """
+
+    def __init__(self, n_inputs, n_neurons):
+        self.name = "Ndk"
+        self.weights = 0.5 * np.random.randn(n_inputs, n_neurons)
+        self.biases = 0.02 * np.random.randn(1, n_neurons)
+        self.activation_function = "relu"
+
+    def validate(self):
+        pass
+
+    def forward(
+        self,
+        X,
+        y,
+        learning_rate,
+        activation_function="relu",
+        loss="categorical",
+        optimizer="gradient",
+    ):
+        weighted_sums = np.dot(X, self.weights)
+
+        self.activation_function = activation_function
+        self.loss = loss
+        self.optimizer = optimizer
+        self.learning_rate = learning_rate
+
+        # Accept either enum values or plain strings from callers.
+        if isinstance(activation_function, str):
+            activation_function = activation_function.lower()
+        elif isinstance(activation_function, Activations):
+            activation_function = activation_function.value
+
+        match activation_function:
+            case Activations.SIGMOID:
+                self.activation_function = Activation_Sigmoid()
+            case Activations.RELU:
+                self.activation_function = Activation_ReLU()
+            case Activations.TANH:
+                self.activation_function = Activation_Tanh()
+            case Activations.LEAKYRELU:
+                self.activation_function = Activation_LeakyReLU()
+            case "sigmoid":
+                self.activation_function = Activation_Sigmoid()
+            case "relu":
+                self.activation_function = Activation_ReLU()
+            case "tanh":
+                self.activation_function = Activation_Tanh()
+            case "leaky_relu":
+                self.activation_function = Activation_LeakyReLU()
+            case _:
+                self.activation_function = Activation_ReLU()
+
+        y_pred = self.activation_function.forward(weighted_sums)
+        return y_pred
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
+from dense.dense_layer import Dense_Layer
+"""
+    Testing 
+"""
 
-'''
-    basically 
-'''
+
+def test1():
+    X = np.array(
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 1, 2],
+        ]
+    )
+    y = [1, 1, 1, 0]
+    layer1 = Dense_Layer(3, 4)
+    pred = layer1.forward(
+        X, y, learning_rate=0.01, activation_function="leaky_relu", loss="gradient"
+    )
+    print(pred)
+
+
 def main():
-    pass 
+    test1()
+
 
 if __name__ == "__main__":
-    main() 
+    main()
